a8f60d0c15fa12a35b9f7cc0593783badcfab624
MaciYelaska <maciyelaska@creighton.edu>
2021-10-03 18:19:59 -0500

    final

--- forms/forReverseStates/forReverseStates.js
+++ forms/forReverseStates/forReverseStates.js
@@ -1,4 +1,4 @@
 let states = ['Nebraska','Iowa','Minnesota','South Dakota','North Dakota','Kansas','Oklahoma','Texas']
-for (var a = stsates.length - 1; a= 0; a--) {
+for (var a = states.length - 1; a>= 0; a--) {
     console.log(states[a]);
 }
