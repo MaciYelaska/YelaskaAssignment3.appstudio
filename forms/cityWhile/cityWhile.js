4cfa56de9c57480da2105954bbed1e0dc2e15772
MaciYelaska <maciyelaska@creighton.edu>
2021-10-03 22:37:29 -0500

    final

---
+++ forms/cityWhile/cityWhile.js
@@ -0,0 +1,18 @@
+let cities = []
+let stop = 'Stop'
+let i = 0
+let text = ""
+city = prompt('Input a city of your choosing (to stop type: Stop)')
+
+
+while (city != 'Stop') {
+  cities.push(city)
+  city = prompt('Enter another city or type Stop to end')
+  }
+ 
+
+let lowerCities = cities.map(cities => cities.toLowerCase())
+while (i < cities.length) {
+  console.log(lowerCities.join('\r\n'))
+  i++
+}
---
+++ forms/forReverseStates/forReverseStates.js
@@ -0,0 +1,12 @@
+let states = ['Nebraska','Iowa','Minnesota','South Dakota','North Dakota','Kansas','Oklahoma','Texas']
+for (var a = states.length - 1; a>= 0; a--) {
+    console.log(states[a]);
+}
+
+let states = ["Nebraska", "Iowa", "Minnesota", "South Dakota", "North Dakota", "Kansas", "Oklahoma", "Texas"]
+
+states.reverse()
+
+for(let i = 0; i < states.length; i++) {
+  console.log(states[i])
+  }
