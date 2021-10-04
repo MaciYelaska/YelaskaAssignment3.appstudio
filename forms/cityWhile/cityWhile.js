a429cbb766b51ca385864883b7c8c46303eac7bb
MaciYelaska <maciyelaska@creighton.edu>
2021-10-03 22:42:46 -0500

    final

--- forms/cityWhile/cityWhile.js
+++ forms/cityWhile/cityWhile.js
@@ -1,16 +1,16 @@
 let cities = []
-let stop = 'Stop'
-let i = 0
-let text = ""
-city = prompt("Enter a city of your choice (to stop type: Stop)")
 
-while (city != 'Stop') {
-    cities.push(city)
-    let city = prompt("Enter another city or type Stop to end")
-    }
-    
-let lowerCities = cities.map(cities => cities.toLowerCaes())
+let i = 0
+
+let doneWithCities = false
+
+while (doneWithCities == false) {
+    let city = prompt("Enter the next city: ")
+    cities.push(city.toLowerCase())
+    doneWithCities = confirm("Are you done with cities? Press OK to Stop")
+}
+
 while (i < cities.length){
-  console.log(lowerCities.join('\r\n'))
+  console.log(cities[i])
   i++
 }
